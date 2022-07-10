f6671cbfaeeca31696837ebba7bbfe165b2ee701
sid <s.sidd707@gmail.com>
2022-07-10 18:38:14 +0545

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import './widgets/screen.dart';
@@ -9,8 +11,8 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       title: 'calculator',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.brown),
-      ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.brown),
+          textTheme: TextTheme(button: TextStyle(fontSize: 20))),
       home: Calculator(),
     );
   }
@@ -23,10 +25,8 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   final num1 = TextEditingController();
-
   final num2 = TextEditingController();
   String n1 = '', n2 = '';
-
   num _c = 0, a = 5, b = 6;
   String _o = ' ';
 
@@ -39,7 +39,7 @@ class _CalculatorState extends State<Calculator> {
 
   void subtract() {
     setState(() {
-      a > b ? _c = a - b : _c = b - a;
+      _c = a - b;
       _o = '-';
     });
   }
@@ -76,83 +76,198 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Calculator')),
-        body: Column(
-          children: <Widget>[
-            Container(
-              child: Column(
-                children: [
-                  Card(
-                    child: Text('$a'),
-                  ),
-                  Card(
-                    child: Text('$_o'),
-                  ),
-                  Card(child: Text('$b')),
-                ],
-              ),
-            ),
-            Container(
-              height: 50,
-              child: Text('Results:  $_c'),
-            ),
-            Column(
-              children: [
-                Row(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Card(
+                elevation: 7,
+                child: Row(
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: Colors.black)),
-                        onPressed: add,
-                        child: Icon(Icons.add)),
-                    OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: Colors.black)),
-                        onPressed: subtract,
-                        child: Text(
-                          "-",
-                          style: TextStyle(fontSize: 30),
-                        )),
-                    OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: Colors.black)),
-                        onPressed: multiply,
-                        child: Text(
-                          '*',
-                          style: TextStyle(fontSize: 30),
-                        )),
-                    OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide(color: Colors.black)),
-                        onPressed: divide,
-                        child: Text('/', style: TextStyle(fontSize: 30)))
+                    Card(
+                      child: Text('$a'),
+                    ),
+                    Card(
+                      child: Text('$_o'),
+                    ),
+                    Card(
+                      child: Text('$b'),
+                    ),
+                    Container(
+                      child: Text(' =  $_c'),
+                    ),
                   ],
                 ),
-                OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: Colors.black)),
-                    onPressed: clear,
-                    child: Text('Clear')),
-                TextField(onChanged: (value) => n1 = value
-                    //controller: num1,
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter a number',
+                  ),
+                  onChanged: (value) => n1 = value
+                  //controller: num1,
+                  ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Enter another number'),
+                controller: num2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 200,
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Colors.black),
+                        ),
+                        onPressed: clear,
+                        child: Text('Clear')),
+                  ),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          side: BorderSide(color: Colors.black)),
+                      onPressed: add,
+                      child: Icon(Icons.add)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
                     ),
-                TextField(
-                  controller: num2,
-                ),
-                TextButton(
-                  onPressed: submit,
-                  child: Text('Submit'),
-                ),
-                // TextField(
-                //   controller: num2,
-                // )
-              ],
-            )
-          ],
-        )
-        // floatingActionButton: FloatingActionButton(
-        //   child: Ic,
-        //   onPressed: add,
-        // )
-        );
+                    child: Text('1'),
+                    onPressed: () {},
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    child: Text('2'),
+                    onPressed: () {},
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('3'),
+                  ),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: StadiumBorder(side: BorderSide(width: 10)),
+                          side: BorderSide(color: Colors.black)),
+                      onPressed: subtract,
+                      child: Text(
+                        "-",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('4'),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('5'),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('6'),
+                  ),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          side: BorderSide(color: Colors.black)),
+                      onPressed: multiply,
+                      child: Text(
+                        '*',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('7'),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('8'),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      elevation: 9,
+                    ),
+                    onPressed: () {},
+                    child: Text('9'),
+                  ),
+                  OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          side: BorderSide(color: Colors.black)),
+                      onPressed: divide,
+                      child: Text('/', style: TextStyle(fontSize: 20))),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 150,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('0'),
+                      style: ButtonStyle(),
+                    ),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        side: BorderSide(color: Colors.black)),
+                    onPressed: () {},
+                    child: Text('.'),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.cyan,
+                      primary: Colors.pink,
+                      shape: StadiumBorder(),
+                    ),
+                    onPressed: submit,
+                    child: Text(
+                      '=',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
